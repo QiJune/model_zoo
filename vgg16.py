4f051ed3d5d63205b12686502d05759778cb2b91
qijun <qijun1994@hotmail.com>
2018-05-08 14:15:52 +0800

    add style check

--- vgg16.py
+++ vgg16.py
@@ -9,11 +9,20 @@ import paddle.fluid as fluid
 import paddle.fluid.core as core
 import functools
 
+
 class VGG16(object):
     def __init__(self, data_set):
         #data_format is "NCHW"
-        self.data_set_dict = {"cifar10": {"class_dim": 10, "data_shape" :[3, 32, 32]},
-                              "flowers": {"class_dim": 102, "data_shape": [3, 224, 224]}}
+        self.data_set_dict = {
+            "cifar10": {
+                "class_dim": 10,
+                "data_shape": [3, 32, 32]
+            },
+            "flowers": {
+                "class_dim": 102,
+                "data_shape": [3, 224, 224]
+            }
+        }
         self.data_set = data_set
         self.class_dim = self.data_set_dict[self.data_set]["class_dim"]
         self.data_shape = self.data_set_dict[self.data_set]["data_shape"]
@@ -30,6 +39,7 @@ class VGG16(object):
                 conv_with_batchnorm=True,
                 conv_batchnorm_drop_rate=dropouts,
                 pool_type='max')
+
         conv1 = conv_block(input, 64, 2, [0.3, 0])
         conv2 = conv_block(conv1, 128, 2, [0.4, 0])
         conv3 = conv_block(conv2, 256, 3, [0.4, 0.4, 0])
@@ -44,7 +54,8 @@ class VGG16(object):
         return fc2
 
     def construct_vgg16_net(self, learning_rate):
-        images = fluid.layers.data(name='pixel', shape=self.data_shape, dtype='float32')
+        images = fluid.layers.data(
+            name='pixel', shape=self.data_shape, dtype='float32')
         label = fluid.layers.data(name='label', shape=[1], dtype='int64')
         net = self.vgg16_bn_drop(images)
         predict = fluid.layers.fc(input=net, size=self.class_dim, act='softmax')
@@ -52,14 +63,14 @@ class VGG16(object):
         self.avg_cost = fluid.layers.mean(x=cost)
         self.accuracy = fluid.layers.accuracy(input=predict, label=label)
 
-        self.inference_program = fluid.default_main_program().clone(for_test=True)
+        self.inference_program = fluid.default_main_program().clone(
+            for_test=True)
 
         optimizer = fluid.optimizer.Adam(learning_rate=learning_rate)
         opts = optimizer.minimize(self.avg_cost)
         fluid.memory_optimize(fluid.default_main_program())
 
         self.train_program = fluid.default_main_program().clone()
-
 
     def train(self, batch_size, device, num_passes):
         place = core.CPUPlace() if device == 'CPU' else core.CUDAPlace(0)
@@ -68,8 +79,8 @@ class VGG16(object):
 
         train_reader = paddle.batch(
             paddle.reader.shuffle(
-                paddle.dataset.cifar.train10()
-                if self.data_set == "cifar10" else paddle.dataset.flowers.train(),
+                paddle.dataset.cifar.train10() if self.data_set == "cifar10"
+                else paddle.dataset.flowers.train(),
                 buf_size=5120),
             batch_size=batch_size)
         iters = 0
@@ -77,22 +88,22 @@ class VGG16(object):
             # train
             start_time = time.time()
             for batch_id, data in enumerate(train_reader()):
-                img_data = np.array(map(lambda x: x[0].reshape(self.data_shape),
-                                        data)).astype("float32")
+                img_data = np.array(
+                    map(lambda x: x[0].reshape(self.data_shape), data)).astype(
+                        "float32")
                 y_data = np.array(map(lambda x: x[1], data)).astype("int64")
                 y_data = y_data.reshape([-1, 1])
 
                 loss, acc = exe.run(self.train_program,
                                     feed={"pixel": img_data,
-                                        "label": y_data},
+                                          "label": y_data},
                                     fetch_list=[self.avg_cost, self.accuracy])
                 iters += 1
-                print(
-                    "Pass = %d, Iters = %d, Loss = %f, Accuracy = %f" %
-                    (pass_id, iters, loss, acc))
+                print("Pass = %d, Iters = %d, Loss = %f, Accuracy = %f" %
+                      (pass_id, iters, loss, acc))
+
 
 if __name__ == "__main__":
     vgg16 = VGG16(data_set="cifar10")
     vgg16.construct_vgg16_net(learning_rate=1e-3)
     vgg16.train(batch_size=128, device="CPU", num_passes=50)
-
